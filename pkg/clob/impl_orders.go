3f3d422d58b4a7752cfb03f78c14e5889d5ef6ff
shenao78 <shenao.78@163.com>
2026-05-02 18:13:30 +0800

    revert print payload

--- pkg/clob/impl_orders.go
+++ pkg/clob/impl_orders.go
@@ -11,7 +11,6 @@ import (
 	"github.com/GoPolymarket/polymarket-go-sdk/pkg/auth"
 	"github.com/GoPolymarket/polymarket-go-sdk/pkg/clob/clobtypes"
 	"github.com/GoPolymarket/polymarket-go-sdk/pkg/types"
-	"github.com/ethereum/go-ethereum/log"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/common/math"
@@ -177,7 +176,6 @@ func signOrderWithCreds(signer auth.Signer, apiKey *auth.APIKey, order *clobtype
 func (c *clientImpl) PostOrder(ctx context.Context, req *clobtypes.SignedOrder) (clobtypes.OrderResponse, error) {
 	var resp clobtypes.OrderResponse
 	payload, err := buildOrderPayload(req)
-	log.Info("post order", "payload", string(payload))
 	if err != nil {
 		return resp, err
 	}
